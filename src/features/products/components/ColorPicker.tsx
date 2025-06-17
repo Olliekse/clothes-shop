6d9d187c998e218bd59408da49a802a94d4de13f
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2025-06-17 15:40:22 +0300

    cleaned up unused variables

--- src/features/products/components/ColorPicker.tsx
+++ src/features/products/components/ColorPicker.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 interface ColorPickerProps {
   colors: string[];
   selectedColor: string | null;
--- src/features/products/components/ProductListItem.tsx
+++ src/features/products/components/ProductListItem.tsx
@@ -21,45 +21,6 @@ interface InventoryItem {
   discount_percentage: number;
 }
 
-const TickIcon = ({ color }: { color: string }) => {
-  const tickColor = color === "white" || color === "beige" ? "black" : "white";
-  return (
-    <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M5.00035 7.58545L9.59655 2.98926L10.3036 3.69636L5.00035 8.99965L1.81836 5.8177L2.52546 5.1106L5.00035 7.58545Z"
-        fill={tickColor}
-      />
-    </svg>
-  );
-};
-
-const DiagonalLine = () => {
-  return (
-    <svg
-      width="16"
-      height="15"
-      viewBox="0 0 16 15"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        x="15.0002"
-        y="-6.10352e-05"
-        width="1"
-        height="20"
-        transform="rotate(45 15.0002 -6.10352e-05)"
-        fill="#525252"
-      />
-    </svg>
-  );
-};
-
 function ProductListItem({
   product,
   productImage,
@@ -80,23 +41,6 @@ function ProductListItem({
         .map((img) => img.color)
     ),
   ];
-
-  function getColorClass(color: string) {
-    const colorMap: { [key: string]: string } = {
-      red: "bg-red-700",
-      blue: "bg-blue-700",
-      green: "bg-green-700",
-      yellow: "bg-yellow-600",
-      orange: "bg-orange-600",
-      pink: "bg-pink-700",
-      purple: "bg-purple-700",
-      black: "bg-black",
-      white: "bg-white border border-gray-300",
-      beige: "bg-amber-100",
-      brown: "bg-amber-900",
-    };
-    return colorMap[color] || "bg-gray-700";
-  }
 
   const defaultInventoryItem = inventoryItems[0];
 
